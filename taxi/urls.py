ea54e0c9d2fca0e8ac91ff7340cfaf35409e1c2d
Illia Mileckiy <illia.miletskyi@oa.edu.ua>
2025-12-29 18:41:53 +0200

    remove_forked_errors

--- taxi/urls.py
+++ taxi/urls.py
@@ -82,11 +82,6 @@ urlpatterns = [
         name="driver-detail"
     ),
     path(
-        "drivers/",
-        DriverListView.as_view(),
-        name="driver-list"
-    ),
-    path(
         "drivers/create/",
         DriverCreateView.as_view(),
         name="driver-create"
